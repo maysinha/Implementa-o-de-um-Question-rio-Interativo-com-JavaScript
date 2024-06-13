db579fc31daf8fcfa548e8d7714a9ae35713e7fb
maysinha <104452246+maysinha@users.noreply.github.com>
2024-06-13 11:25:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const perguntas = [
     {
-    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+    enunciado: "Atualmento o mundo vive na era da tecnologia.Isso ao longo prazo na sua opinião, sera bom ou ruim?",
     alternativas: [
-    "Isso é assustador!",
-    "Isso é maravilhoso!"
+    "Isso é bom!",
+    "Isso é ruim!"
     ]
     },
     {
-    enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+    enunciado: "Você se considera uma pessoa a favor da tecnologia no ambiente escolar?",
     alternativas: [
-    "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-    "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+    "Sim",
+    "Não"
     ]
     },
     {
-    enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+    enunciado: "Considerando as crianças, você acredita que a tecnologia afeta o desenvolvimento delas?",
     alternativas: [
-    "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-    "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+    "Defendo a ideia de que não tem malefício algum! ",
+    "Me preocupo com as crianças, pois, os pais cada vez mais tem períodos menores para cuidar de seus filhos e acompanhar oque estão fazendo"
     ]
     },
     {
-    enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+    enunciado: "Você considera que a tecnologia vem mudando o comportamento das pessoas com a família?",
     alternativas: [
-    "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-    "Criar uma imagem utilizando um gerador de imagem de IA."
+    "Não, quanto mais distância melhor!",
+    "Sim, os costumes vem sendo deixados de lado pelas telas"
     ]
     
     },
     {
-    enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+    enunciado: "Atualmente, você acha que a tecnologia vem para facilitar o dia a dia?",
     alternativas: [
-    "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-    "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+    "Sim, com toda certeza!",
+    "Não, antes tudo era mais fácil"
     ]
     }
     ];
